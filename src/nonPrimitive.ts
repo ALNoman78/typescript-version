499075fa50de75ae70327eabe6d128f61f13fff0
ALNoman78 <abdullaalno078@gmail.com>
2025-11-16 22:28:10 +0600

    non-primitive data type declare

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,11 @@
+// todo Non-Primitive Data type [ array, object ]
+
+// ? string type Array List 
+
+let bazarList: string[] = [" eggs", "milk", "sugar"]
+
+// bazarList.push(true) doesn't work because when i already declare  the array should be in string 
+
+let mixedArr : (string | number)[] = ["eggs" , 12 , "milk", 1 , "sugar", 2] // here i also used mixed array like number and array
+
+mixedArr.push(12)
