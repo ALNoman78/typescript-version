9d3828dcf75ef4a1b8af6f52dcab57b079f18338
ALNoman78 <abdullaalno078@gmail.com>
2025-11-16 21:06:26 +0600

    primitive type is typescript

---
+++ src/primitive.ts
@@ -0,0 +1,22 @@
+// JS -TS
+// string, number, boolean, undefined, null
+
+//? TS: never, unknown, void [ that's all primitive data type is typescript]
+
+//todo string type 
+
+let userName: string = "noman"
+
+userName = "noman078"
+
+//todo  type Number 
+
+let userId : number = 123
+
+userId = 213
+
+//todo  type of boolean [ inplicit & ]
+
+let isAdmin : boolean = false
+
+isAdmin = true
