ec09af7531f5d144898c2d47f15ce8e85119985f
ALNoman78 <abdullaalno078@gmail.com>
2025-11-17 21:20:26 +0600

    touple type in typescript

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -6,6 +6,24 @@ let bazarList: string[] = [" eggs", "milk", "sugar"]
 
 // bazarList.push(true) doesn't work because when i already declare  the array should be in string 
 
-let mixedArr : (string | number)[] = ["eggs" , 12 , "milk", 1 , "sugar", 2] // here i also used mixed array like number and array
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2] // here i also used mixed array like number and array
 
 mixedArr.push(12)
+
+let mixedArrTwo: (string | number)[] = ["Noman", 21, "Person 2", 2, "person 3", 3]
+
+mixedArrTwo.push(2)
+
+// todo typescript -> tuple 
+
+let coordinates: [number, number] = [10, 20] //? tuple is just like couple 
+
+let couple: [string, string] = ["husband ", "wife"]
+
+let nameAndRoll: [string, number] = ["Person 1", 21] //! The main rule is strict here like if i assign the tuple as a string first the i have to use there string first 
+
+// nameAndRoll[0] = 32 //! that's an Error here
+
+// ? here is an example
+
+let destination: [string, string, number] = ["Dhaka", "Kurigram", 3]
