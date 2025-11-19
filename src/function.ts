c4fa453b64bbfc42e1042d0b63b8725ec57febe7
ALNoman78 <abdullaalno078@gmail.com>
2025-11-19 22:43:03 +0600

    normal way to define function

---
+++ src/function.ts
@@ -0,0 +1,15 @@
+// todo Function
+// arrow function, normal function
+
+function addNormal(num1 : number, num2 : number ) : number {
+    return num1 + num2
+}
+
+// console.log(addNormal(2, 2));
+
+
+const addArrow = (number1 : number, number2 : number) : number => {
+    return number1 + number2
+}
+
+console.log(addArrow(2,3))
