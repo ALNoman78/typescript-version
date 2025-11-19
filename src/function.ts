398d56da5efe108164e85ce92a7f6d5c417a3cec
ALNoman78 <abdullaalno078@gmail.com>
2025-11-19 22:53:20 +0600

    object method

--- src/function.ts
+++ src/function.ts
@@ -12,4 +12,20 @@ const addArrow = (number1 : number, number2 : number) : number => {
     return number1 + number2
 }
 
-console.log(addArrow(2,3))
+// console.log(addArrow(2,3))
+
+
+// todo Object => function => method [when a function is used in object it called Method ]
+
+const poorUser = {
+    name: "Noman",
+    balance: 0,
+    addBalance(value: number): number {
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    }
+}
+
+// console.log(poorUser.addBalance(1000))
+
+//todo use function in loop it's called "Call Back function"
