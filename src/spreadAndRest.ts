89e9dd2d42520d7e5ac94bff1b5470c3491b9b47
ALNoman78 <abdullaalno078@gmail.com>
2025-11-20 23:24:58 +0600

    rest operator start

--- src/spreadAndRest.ts
+++ src/spreadAndRest.ts
@@ -7,6 +7,24 @@ const schoolFriend = ["Person3", "Person4", "Person5"]
 
 const collageFriend = ["Mr. Person1", "Mr. Person2"]
 
-const test = friends.push(...schoolFriend)
+friends.push(...schoolFriend)
+friends.push(...collageFriend)
 
-console.log(test);
+console.log(friends);
+
+const user = {
+    name: "Moule",
+    phoneNumber: "01318969328"
+}
+
+const otherInfo = {
+    hobby: "Outing",
+    favColor: "black"
+}
+
+const userInfo = { ...user, ...otherInfo }
+
+console.log(userInfo) // ? user marge
+
+
+??
