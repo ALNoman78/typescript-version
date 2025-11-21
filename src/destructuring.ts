55298abed2ff9d394477e8532ced085f7043cac2
ALNoman78 <abdullaalno078@gmail.com>
2025-11-21 23:56:36 +0600

    destructure completed

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -16,4 +16,18 @@ const user = {
 
 const { favoriteColor: myFavoriteColor, name: { middleName: myMiddleName } } = user // it's called ame elis
 
-console.log(myMiddleName)
+// console.log(myMiddleName)
+
+// todo Array Destructuring in Typescript
+
+const friends = ["Person1", "Person2", "Person3"]
+
+// ? Before Destructure
+
+// const myFriend = friends[0] # normal way to define destructure
+
+// ? After Destructure
+
+const [A, myBestFriend, C] = friends //! const [, myBestFriend] if i don't need the first index or anything just use a empty coma I also can skip the last number of Data
+
+console.log(myBestFriend)
