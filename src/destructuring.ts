d4b57c83e77d91f36e70f702a4e620d27d769d2a
ALNoman78 <abdullaalno078@gmail.com>
2025-11-21 23:45:07 +0600

    object destructuring completed

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -1,1 +1,19 @@
-console.log("destructure connecting")
+// todo  Object destructure
+// todo Array destructure
+
+const user = {
+    userUid: 123,
+    name: {
+        firstName: "Abdulla",
+        middleName: "Al",
+        lastName: "Noman",
+    },
+    gender: "male",
+    favoriteColor: "black",
+}
+
+// const myFavoriteColor = user.favoriteColor
+
+const { favoriteColor: myFavoriteColor, name: { middleName: myMiddleName } } = user // it's called ame elis
+
+console.log(myMiddleName)
